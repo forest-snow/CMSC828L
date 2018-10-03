1c01805fd7786c62f93ba02dd9bb8db6a3624729
Michelle Yuan <myuan@clipsub00.umiacs.umd.edu>
2018-10-03 10:07:45 -0400

    finished mnist

--- Problem Set 2/MNIST/model.py
+++ Problem Set 2/MNIST/model.py
@@ -22,10 +22,10 @@ def load_data():
     x_test = np.load('testImages.npy')
     y_test = np.load('testLabels.npy')
     # reshape
-    x_train = x_train.reshape(x_train.shape[0], n_pixels).astype('float32')
-    x_test = x_test.reshape(x_test.shape[0], n_pixels).astype('float32')
-    # x_train = x_train.reshape(x_train.shape[0], 28, 28, 1).astype('float32')
-    # x_test = x_test.reshape(x_test.shape[0], 28, 28, 1).astype('float32')
+    # x_train = x_train.reshape(x_train.shape[0], n_pixels).astype('float32')
+    # x_test = x_test.reshape(x_test.shape[0], n_pixels).astype('float32')
+    x_train = x_train.reshape(x_train.shape[0], 28, 28, 1).astype('float32')
+    x_test = x_test.reshape(x_test.shape[0], 28, 28, 1).astype('float32')
 
     # normalize
     x_train = x_train/255.0
@@ -33,10 +33,22 @@ def load_data():
 
     return x_train, y_train, x_test, y_test
 
-def build_model():
+def simple_model():
     model = Sequential()
     model.add(Dense(n_pixels, input_dim=n_pixels, kernel_initializer='normal', activation='relu'))
     model.add(Dense(n_classes, kernel_initializer='normal', activation='softmax'))
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+def cnn_model():
+    model = Sequential()
+    model.add(Conv2D(32, (5, 5), input_shape=(28, 28, 1), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(n_classes, activation='softmax'))
     # Compile model
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
@@ -100,9 +112,9 @@ if __name__ == '__main__':
     print('load data')
     x_train, y_train, x_test, y_test = load_data()
     print('build model')
-    model = build_model()
+    model = cnn_model()
     print('fit model')
-    history = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=1, batch_size=2000, verbose=2)
+    history = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=5, batch_size=100, verbose=2)
     plot_scores(history.history)
     plot_weights_biases(model)
     find_errors(model, x_test, y_test)
--- Problem Set 2/MNIST/run.sh
+++ Problem Set 2/MNIST/run.sh
@@ -3,4 +3,4 @@
 #SBATCH --mem=MaxMemPerNode
 #SBATCH --time=02:00:00
 
-python mnist.py
+python model.py
